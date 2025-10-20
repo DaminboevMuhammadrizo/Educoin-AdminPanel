34d1d9c567282eb345599e284852e51ef8d45141
muhammadrizo <muhammadrizo@kali.kali>
2025-10-20 23:47:49 +0500

    updated-2

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    images: {
-        domains: ['cdn-icons-png.flaticon.com'],
-        formats: ['image/webp', 'image/avif'],
-        minimumCacheTTL: 60,
-    },
-    compiler: {
-        removeConsole: process.env.NODE_ENV === 'production',
-    },
-    experimental: {
-        optimizeCss: true,
-    },
-    // swcMinify ni experimental dan chiqarib, asosiy config ga qo'ying
-    swcMinify: true,
-    compress: true,
-    trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['cdn-icons-png.flaticon.com'],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  swcMinify: true,
+  compress: true,
+  trailingSlash: true,
 }
 
 export default nextConfig
